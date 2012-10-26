c4444ee8cd232884d5f0c72ab78bc1c6e28ac13c
Florian Eibeck <florian.eibeck@mayflower.de>
2012-10-26 19:55:01 +0200

    Cleaned up the configuration files

--- config/application.config.php
+++ config/application.config.php
@@ -18,25 +18,4 @@ return array(
             './vendor',
         ),
     ),
-//    'doctrine' => array(
-//        'driver' => array(
-//            'default_driver' => array(
-//                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-//                'cache' => 'array',
-//                'paths' => array(__DIR__ . '/../module/Todo/src/Todo/Entity')
-//            ),
-//            'orm_default' => array(
-//                'drivers' => array(
-//                    'Todo\Entity' => 'default_driver'
-//                )
-//            )
-//        ),
-//        'connection' => array(
-//            'orm_default' => array()
-//        )
-//    )
-
-
-
-
 );
